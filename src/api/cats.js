98200f0550d6e736e00fdd51151f31e860d104a1
Pano Papadatos <pano.papadatos@publicrelay.com>
2018-02-10 19:16:32 -0500

    Production Build as gh-pages

--- src/api/cats.js
+++
@@ -1,5 +0,0 @@
-import service from '../util/service';
-
-export function getCats(){
-    return service.get("cats");
-}
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import numeral from 'numeral';
-import {getCats} from "./api/cats";
-import './scss/index.scss';
-
-
-const value = numeral(1000).format('$0,0.00');
-console.log(`Value is ${value}`);
-
-getCats().then(result => console.log(result));
